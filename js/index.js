1e0acd4ae1aea0d6e16619e51f2bb44624082345
derek-cassady <derek@cassadyweb.com>
2025-01-11 22:54:14 -0500

    Assignment complete.  Missed the part about pushing a commit for each step.

--- js/index.js
+++ js/index.js
@@ -2,3 +2,38 @@
 // that are not declared
 "use strict"
 
+// console.log(10 + 5)        // Output: 15
+
+// console.log(10 - 5)         // Output: 5
+
+// console.log(10 + "5")      // Output: 105
+
+// console.log(20 / 4)          // Output: 5
+
+// console.log(20 % 3)          // Output: 2
+
+// console.log(2 * 3 + 4)       // Output: 10
+
+// console.log(2 * (3 + 4))     // Output: 14
+
+// console.log(5 == "5")       // Output: true
+
+// console.log(5 === "5")      // Output: false
+
+// console.log(true || false)   // Output: true
+
+// console.log(false || true)   // Output: true
+
+// console.log(true && false)   // Output: false
+
+// console.log(true && true)    // Output: true
+
+// console.log(!true)           // Output: false
+
+// console.log(7 > 5)           // Output: true
+
+// console.log(7 < 5)           // Output: false
+
+// console.log(7 >= 7)          // Output: true
+
+// console.log(7 <= 6)          // Output: false
